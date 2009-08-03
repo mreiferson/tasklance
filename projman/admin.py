5a68ae0fff9426c31db1cc95620e6ab20f01143a
Matt Reiferson <snakes@gmail.com>
2009-08-03 00:02:13 +0000

    updated projman
    git-svn-id: https://svn.vrenesis.com/tasklance/trunk@3368 e4e18489-d04f-4dbd-bd3b-2e29156f5e3f

--- projman/admin.py
+++
@@ -1,9 +0,0 @@
-from pm.projman.models import *
-from django.contrib import admin
-
-admin.site.register(UserAccount)
-admin.site.register(Account)
-admin.site.register(Project)
-admin.site.register(Category)
-admin.site.register(Todo)
-admin.site.register(Dependency)
