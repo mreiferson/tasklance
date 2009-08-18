d1c353d80b5a84bea3cd97b5506f56a51a4743b5
Matt Reiferson <snakes@gmail.com>
2009-08-18 02:16:16 +0000

    added another template tag
    git-svn-id: https://svn.vrenesis.com/tasklance/trunk@3393 e4e18489-d04f-4dbd-bd3b-2e29156f5e3f

---
+++ apps/projman/templatetags/custom_tags.py
@@ -0,0 +1,20 @@
+from projman.models import Todo
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag('show_todos.html')
+def show_todos(category, complete=0):
+	todos = Todo.objects.filter(category=category.id, complete=complete).order_by('-completed' if complete else 'priority')
+
+	if complete:
+		todos = todos[:5]
+
+	return locals()
+
+
+@register.inclusion_tag('show_history.html')
+def show_history(project):
+	todos = Todo.objects.filter(category__project__exact=project).filter(complete=1).order_by('-completed')
+	
+	return locals()
